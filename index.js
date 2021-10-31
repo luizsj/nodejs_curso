aafa2771c9ec7c6a461d61743440474de4b77557
LuizSoares <luiz.jesus@fatec.sp.gov.br>
2021-10-31 09:35:52 -0300

    aula_20 start layout jandlebars

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
-const Sequelize = require('sequelize')
+const Sequelize = require('sequelize');
 
 //Config
     // Template Engine
-        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-        app.set('view engine', 'handlebars')
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+        app.set('view engine', 'handlebars');
     // conexao banco de dados
         const sequelize = new Sequelize('test', 'root', 'm6rpt2m6'
             , { host: "localhost"
                 , dialect: 'mysql'
             });
-
+//Rotas
+    app.get('/cad', function(req, res){
+        res.render('formulario')
+    });
 
 app.listen(8080, function(){
     console.log('Servidor rodando porta 8080');
