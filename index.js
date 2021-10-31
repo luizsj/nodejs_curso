b703a27927362e8061ea2af69ebe414bb04bea11
LuizSoares <luiz.jesus@fatec.sp.gov.br>
2021-10-31 10:09:12 -0300

    aula_22  basic form post

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
 
 //Config
     // Template Engine
         app.engine('handlebars', handlebars({defaultLayout: 'main'}));
         app.set('view engine', 'handlebars');
+    //Body Parser
+        app.use(bodyParser.urlencoded({extended: false}));
+        app.use(bodyParser.json());
     // conexao banco de dados
         const sequelize = new Sequelize('test', 'root', 'm6rpt2m6'
             , { host: "localhost"
                 , dialect: 'mysql'
             });
+
 //Rotas
     app.get('/cad', function(req, res){
         res.render('formulario')
     });
     app.post('/form_postagem', function(req, res){
-        res.send('formulário recebido')
+        res.send('formulário recebido<br>'
+                +'Texto: '+req.body.titulo + '<br>'
+                +'Conteudo: '+req.body.conteudo
+                )
     })
 
 app.listen(8080, function(){
